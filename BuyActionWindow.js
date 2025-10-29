6b88e9f8f05e9977f42751eb9ccc9f676e169366
RAJ SINGH <raajsingh8879@gmail.com>
2025-10-29 21:38:21 +0530

    Dashboard
    Here's I upload full files which I used in dashboard.

---
+++ BuyActionWindow.js
@@ -0,0 +1,46 @@
+import React,{useState} from "react";
+import {Link} from "react-router-dom";
+import axios from "axios";
+import GeneralContext from "./GeneralContext";
+import "./BuyActionWindow.css";
+
+const BuyActionWindow = ({uid}) => {
+    const [stockQuantity, setStockQuanity] = useState(1);
+    const [stockPrice, setStockPrice] = useState(0.0);
+    const handleBuyClick =  () => {
+        axios.post("http://localhost:3002/newOrder", {
+            name: uid,
+            qty: stockQuantity,
+            price: stockPrice,
+            mode: "BUY",
+        });
+        GeneralContext.closeBuyWindow();
+    };
+    const handleCancelClick = () => {
+        GeneralContext.closeBuyWindow();
+    };
+    return (
+        <div className="containerClass" id="buy-window" draggable="true">
+            <div className="regular-rder">
+                <div className="inputs">
+                    <fieldset>
+                        <legend>Qty</legend>
+                        <input type="number" name="qty" id="qty" onChange={(e) => setStockQuanity(e.target.value)} value={stockQuantity}/>
+                    </fieldset>
+                    <fieldset>
+                        <legend>Price</legend>
+                        <input type="number" name="price" id="price" step="0.05" onChange={(e) => setStockPrice(e.target.value)} value={stockPrice}/>
+                    </fieldset>
+                </div>
+            </div>
+            <div className="buttons">
+                <span>Margin required $140.65</span>
+                <div>
+                    <Link className="btn btn-blue" onClick = {handleBuyClick}>Buy</Link>
+                    <Link className="btn btn-grey" onClick={handleCancelClick}>Cancel</Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default BuyActionWindow;
